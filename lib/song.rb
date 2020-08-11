23c5bb8fc0464b1d7adb7985afed4df748c9821a
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-11 18:02:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,7 +19,7 @@ class Song
     file = filename.chomp(".mp3").split(" - ")
     song = self.new(file[1])
     artist = self.new(file[0])
-    @artist.name = artist
+    @artist = artist
     song
     
   end
