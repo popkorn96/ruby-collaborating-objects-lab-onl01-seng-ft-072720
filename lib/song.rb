0478c79a2ee22e6c3cb6710c99d0c92f239ebe4d
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-11 18:18:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,13 +12,16 @@ class Song
   #  associates new song instance with the artist from the filename (FAILED - 2)
   
  
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    @artist.add_song(self)
+  def artist_name=(artist_name)
+    if Artist.all.detect{|name| name.name -- artist_name}
+      self.artist = Artist.find_or_create_by_name(name)
+    else 
+      self.artist = Artist.new(artist_name)
+    end
   end
   def self.new_by_filename(filename)
     song = filename.chomp(".mp3").split(" - ")[1]
-    artist_name = self.new(file[1])
+    artist_name = filename.chomp(".mp3").split(" - ")[0]
     artist_name = self.new(file[0])
     artist_name = @artist
     self.artist = artist_name
